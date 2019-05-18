ec4e42326982bcb2890747bbf969eb12baf6d79b
miaohj <miaohj@buaa.edu.cn>
2019-05-18 12:14:41 +0800

    Initial commit

---
+++ src/main/java/com/myquora/myquora/MyquoraApplication.java
@@ -0,0 +1,13 @@
+package com.myquora.myquora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyquoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyquoraApplication.class, args);
+	}
+
+}
